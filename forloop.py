7f64d544e9baa7fbae1aff79f6565bb4bc398a47
Mabudu Jabateh <wiseobinnamj33@gmai.com>
2024-12-25 23:26:35 +0000

    part2

---
+++ forloop.py
@@ -0,0 +1,16 @@
+prices= [20,4,24]
+total= 0
+for price in prices: 
+    total+= price 
+print(f'sum: {total}')
+print('This is a new program!!')
+for x in range(5): 
+    for y in range(3): 
+        print(f'({x},{y})')
+print('This is  a new program:  ')
+number= [5,2,5,2,2]
+for num in number: 
+    print('x'*num)
+print('New program: ')
+item= ['John', 'Fatu', 'Alieu']
+print(item[0])
